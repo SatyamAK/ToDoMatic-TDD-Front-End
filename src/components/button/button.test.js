11b0acc31f11f556a4fc763b0b142e4f3adfa0b9
SatyamAK <satyam.ak99@gmail.com>
2022-07-25 19:02:26 +0530

    Changed the import for renamed index.js

--- src/components/button/button.test.js
+++ src/components/button/button.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import Button from "./button";
+import Button from "./index"
 
 describe('Button', ()=>{
 
