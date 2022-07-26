849d818c7512270036f9ce2a9d277db45a1ff522
SatyamAK <satyam.ak99@gmail.com>
2022-07-26 13:36:12 +0530

    Unit tests which I forgot to do previously done

--- src/components/text-form-field/index.js
+++ src/components/text-form-field/index.js
@@ -1,7 +1,17 @@
 import './style.css'
 
 export default function TextFormField(props){
+
+    function onChange(event){
+        props.onChange()
+    }
+
     return (
-        <input type="text" className='text-form-field' placeholder={props.placeholder}/>
+        <input 
+            type={props.type} 
+            className='text-form-field' 
+            placeholder={props.placeholder}
+            value={props.value}
+            onChange = {onChange}/>
     )
 }
--- src/components/text-form-field/text-form-field.test.js
+++ src/components/text-form-field/text-form-field.test.js
@@ -1,13 +1,39 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import TextFormField from ".";
 
 describe('TextFormField Tests', () => {
+
+    let onChange = jest.fn()
 
     test('TextFormField should have passed placeholder text through props', () => {
 
         let placeHolder = "What should be task?"
 
-        render(<TextFormField placeholder={placeHolder} />)
+        render(<TextFormField placeholder={placeHolder} value="" onChange = {onChange}/>)
         expect(screen.getByPlaceholderText(placeHolder)).toBeInTheDocument()
+    })
+
+    test('TextFormField should have type passed through props', () => {
+
+        let type = "password"
+
+        render(<TextFormField placeholder={'placeHolder'} type = {type} value="" onChange = {onChange}/>)
+        expect(screen.getByPlaceholderText('placeHolder')).toHaveAttribute("type", type)
+    })
+
+    test('TextFormField should have value passed through props', () => {
+
+        let value = "user"
+
+        render(<TextFormField placeholder={'placeHolder'} value = {value} onChange = {onChange}/>)
+        expect(screen.getByPlaceholderText('placeHolder')).toHaveAttribute("value", value)
+    })
+
+    test('Onchange function should call fuction passed through props', ()=>{
+
+        render(<TextFormField placeholder={'placeHolder'} value = {'value'} onChange = {onChange}/>)
+        userEvent.type(screen.getByPlaceholderText('placeHolder'), "abc")
+        expect(onChange).toBeCalled()
     })
 })
